ae3da4f516de462161a40793cbc6aa86acf081d6
Anmoldeep <anmoldeepsingh.lnu1@rdpolytech.ca>
2025-04-01 11:18:20 -0600

    update

---
+++ routes/users.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController.js');
+
+
+
+//render our home page
+router.get('/', (req, res)=>{
+    res.render('home');
+});
+
+//post route handler to create a new user
+router.post('/users', userController.createUser);
+
+
+module.exports = router;
